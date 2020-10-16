960f0049c719289fd910e45c5edb3489e423fbe5
ksy <siwon2301@naver.com>
2020-10-16 17:46:04 +0900

    10월 16일 수업 진행 백업

---
+++ TypeEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,49 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		 /*
+		  * 변수
+		자료형(type)
+
+기본자료형			참조자료형
+(primitive)			(referece)
+
+boolean : true, false		-기본자료형이 아닌 자료형
+boolean a = true;
+boolean b = false;
+
+char : 한 글자
+char a = '한'
+
+
+정수형
+
+byte
+-128 ~ 127 까지 표현 가능
+
+short
+거의 안 쓰므로 무시
+
+int (정수형의 기본 자료형)
++-21억
+
+long 
+long aaa = 2500000000;
+long형의 맞는 값은 숫자 뒤에 L/l 을 써줘야 한다.
+
+엄청 큰 정수를 사용하고 싶으면 Biginteger
+
+
+실수형 (정밀도)
+
+float : 값 뒤에 F/f 를 써줘야 한다.
+
+double 매우 정밀하게 표현 (실수형의 기본 자료형)
+
+() bbb = 3.14*/
+		
+		
+		
+	}
+}
---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,31 @@
+package kr.co.ezen;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+
+		String msg = "hello";
+		String msg2 = "hi";
+		String msg3 = "good";
+		String msg4 = "morning";
+		String msg5 = "googoo";
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println(msg4);
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		System.out.println(msg5);
+		
+		
+		// 자료형 변수명 = 자료형에맞는값;
+		// 값에맞는자료형 변수명 = 값;
+	
+
+		long aaa = 2500000000L;
+		System.out.println(aaa);
+	}
+}
